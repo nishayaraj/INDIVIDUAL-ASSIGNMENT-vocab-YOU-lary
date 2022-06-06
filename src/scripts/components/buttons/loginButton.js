5e2433ba9a0708aebd392fcddc8b0d1b6c913cf0
nishaya raj <nishayaraj@gmail.com>
2022-06-06 10:07:01 -0500

    created sample data,login page

--- src/scripts/components/buttons/loginButton.js
+++ src/scripts/components/buttons/loginButton.js
@@ -1,8 +1,9 @@
-import signIn from '../helpers/signIn';
+import signIn from '../../helpers/signIn';
 
 // GOOGLE LOGIN BUTTON
 const loginButton = () => {
-  const domString = '<button id="google-auth" class="btn btn-danger">GOOGLE LOGIN</button>';
+  const domString = `<h2>Welcome to Vocab-You-Lary</h2>
+  <button id="google-auth" class="btn btn-danger">GOOGLE LOGIN</button>`;
   document.querySelector('#login-form-container').innerHTML = domString;
   document.querySelector('#google-auth').addEventListener('click', signIn);
 };
